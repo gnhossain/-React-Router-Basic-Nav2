c1e64789d9db7198da5d86791c1994ece816706f
G H <gh@Gs-MacBook-Pro.local>
2020-02-24 12:48:03 -0800

    fixed navigation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import { Home, About, Contact, Navigation } from './components';
 import { Route } from "react-router-dom";
 const App = () => (
   <div>
+    <div>
     <Navigation />
+    </div>
+    <div>
+    {/* Adding Route to the path */}
     <Route exact path="/" component={Home}/>
     <Route path="/contact" component={Contact}/>
     <Route path="/about" component={About}/>
+    </div>
   </div>
 );
 
